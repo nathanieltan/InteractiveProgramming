9cef52e84420eccd5bec96b4d9f498c8f6842469
camillexue <xue.camille@gmail.com>
2017-02-27 15:16:05 -0500

    started adding accelaration

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 from pygame.locals import *
 import os, sys
-from euclid import *
+import math
+clock = pygame.time.Clock()
+
 
 class FunGameMain:
     """
@@ -26,6 +28,10 @@ class FunGameMain:
         self.background.fill((0, 0, 0))
 
         while 1:
+            dtime_ms = clock.tick(60)  # gets the tick time in milliseconds
+            dt = dtime_ms/1000  # converting the tick time to seconds
+
+            self.smiley.decel()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.display.quit()
@@ -35,8 +41,9 @@ class FunGameMain:
                        or (event.key == K_LEFT)
                        or (event.key == K_UP)
                        or (event.key == K_DOWN)):
-                        self.smiley.move(event.key)
+                        self.smiley.keyPress(event.key)
 
+            self.smiley.move(dt)
             # drawing
 
             self.screen.blit(self.background, (0, 0))
@@ -47,36 +54,40 @@ class FunGameMain:
         self.smiley = Smiley()
         self.smiley_sprites = pygame.sprite.RenderPlain((self.smiley))
 
- 
+
 class Smiley(pygame.sprite.Sprite):
     """ make smiley """
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        image = pygame.image.load('smiley.png')
+        name = os.path.join('smiley.png')
+        image = pygame.image.load(name)
         self.image = image.convert()
         self.rect = image.get_rect()
-        self.x_dist = 64
-        self.y_dist = 64
         self.dx = 0
         self.dy = 0
         self.ddx = 0
         self.ddy = 0
 
-    def move(self, key):
-        x_move = 0
-        y_move = 0
+    def move(self, dt):
+        self.rect.move_ip(self.dx * dt, self.dy * dt)
+        self.dx += self.ddx
+        self.dy += self.ddy
 
+    def decel(self):
+        if self.dx != 0:
+            self.ddx = -1 * self.dx * .01
+
+    def keyPress(self, key):  # handles the efffects of a key press
         if (key == K_RIGHT):
-            x_move = self.x_dist
+            if self.dx < 10:
+                self.ddx = 10
         elif (key == K_LEFT):
-            x_move = -self.x_dist
+            if self.dx > -10:
+                self.ddx = -10
         elif (key == K_UP):
-            y_move = -self.y_dist
+            self.dy = 50
         elif (key == K_DOWN):
-            y_move = self.y_dist
-
-        self.rect.move_ip(x_move, y_move)
-
+            pass
 
 if __name__ == "__main__":
     MainWindow = FunGameMain()
